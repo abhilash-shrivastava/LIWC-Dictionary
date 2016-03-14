023d8ed93cd11ce7a9cfc99f12a41df49aab3f53
Abhilash Shrivastava <abhilash.shrivastava@hotmail.com>
2016-03-14 02:01:53 -0700

    Reading positive emotion words from unix dictionary

--- LIWC.py
+++ LIWC.py
@@ -1,7 +1,32 @@
 with open("Posemo.txt") as f:
-    content = f.readlines();
+    content = f.readlines()
 
+    stems = []
     for line in content:
-        stem = line.split("\n")
-        stem = stem[0].split("*")
-        print stem[0];
+        line = line.split("\n")
+        line = line[0].split("*")
+        stems.append(line[0]);
+    # print stem
+
+with open("words") as f:
+    content = f.readlines()
+
+    unix = []
+    for line in content:
+        line = line.split("\n")
+        line = line[0].split("*")
+        unix.append(line[0]);
+    # print unix
+# Open a file
+    posWords = open("PosWords.txt", "wb")
+
+
+    import re
+    for stem in stems:
+        pattern = '(.*)'+stem+'(.*)'
+        indices = [i for i, x in enumerate(unix) if re.search(pattern, x)]
+        print indices
+        for index in indices:
+            posWords.write(unix[index]+'\n');
+            print unix[index]
+
