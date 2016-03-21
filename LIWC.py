5a9af6b398250b4dbb140c0d3e2c3ecf7faefe51
Abhilash Shrivastava <abhilash.shrivastava@hotmail.com>
2016-03-21 04:07:50 -0700

    Generated TriGrams

--- LIWC.py
+++ LIWC.py
@@ -5,7 +5,7 @@ with open("Posemo.txt") as f:
     for line in posContent:
         line = line.split("\n")
         line = line[0].split("*")
-        posStems.append(line[0]);
+        posStems.append(line[0].upper());
 
 with open("Negemo.txt") as f:
     negContent = f.readlines()
@@ -14,7 +14,7 @@ with open("Negemo.txt") as f:
     for line in negContent:
         line = line.split("\n")
         line = line[0].split("*")
-        negStems.append(line[0]);
+        negStems.append(line[0].upper());
 
 with open("words") as f:
     content = f.readlines()
@@ -23,14 +23,14 @@ with open("words") as f:
     for line in content:
         line = line.split("\n")
         line = line[0].split("*")
-        unix.append(line[0]);
+        unix.append(line[0].upper());
 
     posWords = open("PosWords.txt", "wb")
 
 
     import re
     for stem in posStems:
-        pattern = '(.*)'+stem+'(.*)'
+        pattern = stem+'(.*)'
         indices = [i for i, x in enumerate(unix) if re.search(pattern, x)]
         for index in indices:
             posWords.write(unix[index]+'\n');
---
+++ TriGrams.py
@@ -0,0 +1,29 @@
+with open("PosWords.txt") as f:
+    posContent = f.readlines()
+
+    posWords = []
+    for num in range(1, 10):
+        posContent[num] = posContent[num].split("\n")
+        posWords.append(posContent[num][0]);
+
+with open("NegWords.txt") as f:
+    negContent = f.readlines()
+
+    negWords = []
+    for num in range(1, 10):
+        negContent[num] = negContent[num].split("\n")
+        negWords.append(negContent[num][0]);
+
+
+    triGrams = open("TriGrams.txt", "wb");
+    for posword in posWords:
+        for negword in negWords:
+            posAndTriGram = posword + " AND " + negword + "\n";
+            triGrams.writelines(posAndTriGram);
+            posOrTriGram = posword + " OR " + negword + "\n";
+            triGrams.writelines(posOrTriGram);
+            negAndTriGram = negword + " AND " + posword + "\n";
+            triGrams.writelines(negAndTriGram);
+            negOrTriGram = negword + " OR " + posword + "\n";
+            triGrams.writelines(negOrTriGram);
+
